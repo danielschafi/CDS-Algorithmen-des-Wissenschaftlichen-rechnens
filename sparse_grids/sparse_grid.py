8420e0dda092c7bb51e0c9faa7dafb2962408ad7
danielschafi <daniel.schafi@bluewin.ch>
2026-02-28 18:47:46 +0100

    rewritten code successfully

--- sparse_grids/sparse_grid.py
+++ sparse_grids/sparse_grid.py
@@ -6,11 +6,18 @@ class SparseGrid:
     def __init__(self, l: int):
         self.l: int = l  # Total layer count
 
+        self.levels = list(range(1, self.l + 1))
+        print(f"levels: {self.levels}")
         # Grid spacing per level
-        self.h = np.array([2 ** (-k) for k in range(self.l)])
-
+        self.h = np.array(
+            [2 ** (-k) for k in range(self.l + 1)]
+        )  # get grid spacing for levels 0 to l, we will only use levels 1 to l, but this way we have the correct index for h[k]
+        print(f"Grid spacing per level: {self.h}")
         # Base function values.
-        self.base_function_values = np.zeros((self.l, 2**self.l + 1))
+        self.base_function_values = np.zeros((self.l + 1, 2**self.l + 1))
+
+        print(f"Base function values shape: {self.base_function_values.shape}")
+        print(self.base_function_values)
 
     def x_li(self, l: int, i: int) -> float:
         """x coordinate of gridpoint at index i on level l"""
@@ -30,7 +37,7 @@ class SparseGrid:
         else:
             return 0
 
-    def phi_li(self, x: float | np.float64, k: int, i: int, h_l: float) -> float:
+    def phi_li(self, x: float | np.float64, k: int, i: int) -> float:
         """
         Base function for gridpoint x_li(l, i) on level l
         with phi_li(x_li) = 1 on [x_li - h_l, x_li + h_l]. 1 in the center and 0 at the borders
@@ -39,11 +46,10 @@ class SparseGrid:
             x: (float) x coordinate
             k: (int) level
             i: (int) index of the grid point of the base function (on level k)
-            h_l: (float) grid spacing on level l
         Returns
             float value of the base function at x
         """
-        return self.phi((x - self.x_li(k, i)) / h_l)
+        return self.phi((x - self.x_li(k, i)) / self.h[k])
 
     def indices_of_funcs_on_k(self, k: int) -> list[int]:
         """
@@ -60,26 +66,25 @@ class SparseGrid:
         Builds the base functions for all levels and saves the values at the resolution of the finest level.
         """
 
-        # Each row in marix corresponds to a level W_k.
-
-        # X coords on the finest level, we have to calculate the values of the base function at these points.
         x_l = np.linspace(0, 1, 2**self.l + 1)
 
-        for k in range(1, self.l):
+        for k in self.levels:
             I_k = self.indices_of_funcs_on_k(k)
+            print(I_k)
 
             for base_func_idx in I_k:
-                for i in range(2**self.l + 1):
-                    self.base_function_values[k, i] = self.phi_li(
-                        float(x_l[i]), k, base_func_idx, self.h[k]
+                for i in range(len(x_l)):
+                    self.base_function_values[k, i] += self.phi_li(
+                        float(x_l[i]), k, base_func_idx
                     )
 
     def visualize_base_functions(self):
         """
         Plots the base functions on different levels.
         """
+        print(self.base_function_values)
         plt.title("Base functions on different levels")
-        for k in range(1, self.l):
+        for k in self.levels:
             plt.plot(
                 np.linspace(0, 1, 2**self.l + 1),
                 self.base_function_values[k],
@@ -89,6 +94,6 @@ class SparseGrid:
         plt.show()
 
 
-sg = SparseGrid(2)
+sg = SparseGrid(4)
 sg.calculate_base_functions()
 sg.visualize_base_functions()
